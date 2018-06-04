314b514552ad4bd5e07ee079ffe94dc61e5e6373
Jingjing Li <lijingjing1008@gmail.com>
2018-06-04 16:10:37 +0800

    Loop to get diag state

--- test/diags.js
+++ test/diags.js
@@ -2,22 +2,30 @@ var should = require('chai').should(),
     expect = require('chai').expect,
     assert = require('chai').assert;
 supertest = require('supertest'),
+    diagBaseUrl = 'http://evanclinuxdev1.eastasia.cloudapp.azure.com:8080/v1/diagnostics',
     api = supertest('http://evanclinuxdev1.eastasia.cloudapp.azure.com:8080/v1'),
-    diagApi = supertest('http://evanclinuxdev1.eastasia.cloudapp.azure.com:8080/v1/diagnostics');
+    diagApi = supertest('http://evanclinuxdev1.eastasia.cloudapp.azure.com:8080/v1/diagnostics'),
+    Loop = require('./loop.js');
+
 
 describe('Diag', function () {
-    var nodes = [];
-    var test = {};
-    var jobId = '';
-    var taskId = '';
-    var jobState = '';
+    let nodes = [];
+    let test = {};
+    let jobId = '';
+    let taskId = '';
+    let jobState = '';
+    let pingpong = {};
+    let ring = {};
+    let timeout = 0;
 
     before((done) => {
         api.get('/nodes')
             .set('Accept', 'application/json')
             .end((err, res) => {
-                res.body.forEach(element => {
-                    nodes.push(element.id);
+                res.body.forEach(e => {
+                    if (e.health == 'OK') {
+                        nodes.push(e.id);
+                    }
                 });
                 done();
             });
@@ -34,13 +42,20 @@ describe('Diag', function () {
             .end((err, res) => {
                 assert.isArray(res.body);
                 if (res.body.length > 0) {
-                    test = res.body[0];
+                    pingpong = res.body.find((e) => {
+                        return e.name == 'pingpong';
+                    });
+                    pingpong.arguments = "[]";
+                    ring = res.body.find((e) => {
+                        return e.name == 'ring';
+                    });
+                    ring.arguments = "[]";
                 }
                 done();
             });
     })
 
-    it('should create a new diag test', (done) => {
+    it('should create a new pingpong diag test', (done) => {
         if (nodes.length > 4) {
             nodes = nodes.slice(0, 4);
         }
@@ -50,7 +65,7 @@ describe('Diag', function () {
                 name: 'diag-BVT-test',
                 targetNodes: nodes,
                 jobType: 'diagnostics',
-                diagnosticTest: test
+                diagnosticTest: pingpong
             })
             .expect(201)
             .end((err, res) => {
@@ -61,17 +76,63 @@ describe('Diag', function () {
             });
     })
 
-    this.timeout(200000);
-    it('should finish a diag test of 4 nodes in 3 minutes', (done) => {
-        setTimeout(function () {
-            diagApi.get('/' + jobId)
-                .set('Accept', 'application/json')
-                .end((err, res) => {
-                    expect(res.body.state).equal('Finished');
-                    done();
-                });
-        }, 180000)
 
+    this.timeout(nodes.length * 30000);
+    it('should get a pinpong diag test result before timeout', (done) => {
+        let startTime = new Date();
+        timeout = nodes.length * 30000;
+        console.log(timeout);
+        let loop = Loop.start(
+            diagBaseUrl + '/' + jobId,
+            {
+                next: (result) => {
+                    result = JSON.parse(result);
+                    console.log(result.state);
+                    let endTime = new Date();
+                    let elapseTime = endTime - startTime;
+                    if (result.state == 'Finished') {
+                        assert.ok(result.state === 'Finished', 'pingpong diag test finished in ' + elapseTime + ' ms.');
+                        done();
+                        return false;
+                    }
+                    else if (result.state == 'Failed') {
+                        assert.ok(result.state === 'Failed', 'pingpong diag test faied in ' + elapseTime + ' ms.');
+                        done();
+                        return false;
+                    }
+
+                    if (elapseTime > timeout) {
+                        assert.fail("actual runtime " + elapseTime + ' ms', "expected time " + timeout + ' ms', "The pingpong diag test doesn't finished in expected time, time elapses: " + elapseTime + ' ms, the max time is ' + timeout + ' ms');
+                        done();
+                        return false;
+                    }
+                    return true;
+                }
+            },
+            10000
+        );
     })
+
+    // it('should create a new ring diag test', (done) => {
+    //     if (nodes.length > 4) {
+    //         nodes = nodes.slice(0, 4);
+    //     }
+    //     diagApi.post('')
+    //         .set('Accept', 'application/json')
+    //         .send({
+    //             name: 'diag-BVT-test',
+    //             targetNodes: nodes,
+    //             jobType: 'diagnostics',
+    //             diagnosticTest: ring
+    //         })
+    //         .expect(201)
+    //         .end((err, res) => {
+    //             expect(res.headers.location).to.include('/v1/diagnostics/');
+    //             let locationData = res.headers.location.split('/');
+    //             jobId = locationData[locationData.length - 1];
+    //             done();
+    //         });
+    // })
+
 });
 
---
+++ test/loop.js
@@ -0,0 +1,44 @@
+const request = require("request");
+module.exports = class Loop {
+    static start(url, observer, interval = 1500) {
+        let looper = { url: url, ended: false };
+
+        let _loop = () => {
+            if (looper.ended) {
+                return;
+            }
+            let ts = new Date().getTime();
+            request(url, function (error, reponse, body) {
+                if (error !== null) {
+                    looper.ended = true;
+                    if (observer.error) {
+                        observer.error(err);
+                    }
+                }
+                if (looper.ended) {
+                    return;
+                }
+                let elapse = new Date().getTime() - ts;
+                let n = observer.next(body);
+                if (!n) {
+                    looper.ended = true;
+                    return;
+                }
+                let delta = interval - elapse;
+                let _interval = delta > 0 ? delta : 0;
+                setTimeout(_loop, _interval);
+            }
+            );
+        };
+        _loop();
+        return looper;
+    }
+
+    static stop(looper) {
+        looper.ended = true;
+    }
+
+    static isStopped(looper) {
+        return looper.ended;
+    }
+}
